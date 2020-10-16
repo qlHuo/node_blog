fc87bd85088bfee9e106455efeff5e6c427a2821
霍青利 <1322346367@qq.com>
2020-10-16 20:53:45 +0800

    完成文章详情页面

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var articleID = require('./routes/index')
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,6 @@ var router = express.Router();
 var crypto = require('crypto');
 var mysql = require('../database');
 
-/* 首页 */
-router.get('/', function (req, res, next) {
-  var query = 'SELECT * FROM article';
-  mysql.query(query, function (err, rows, fields) {
-    var articles = rows;
-    articles.forEach(ele => {
-      var year = ele.articleTime.getFullYear();
-      var month = ele.articleTime.getMonth() + 1 > 10 ? ele.articleTime.getMonth() : '0' + ele.articleTime.getMonth();
-      var date = ele.articleTime.getDate() + 1 > 10 ? ele.articleTime.getDate() : '0' + ele.articleTime.getDate();
-      ele.articleTime = year + '-' + month + '-' + date
-    });
-    res.render('index', { articles: articles })
-  })
-});
 // 登录页
 router.get('/login', function (req, res, next) {
   res.render('login', { message: '' })
@@ -42,6 +28,47 @@ router.post('/login', function (req, res, next) {
     // req.session.userSign = true;
     // req.session.userID = user.authorID
     res.redirect('/');
+  })
+})
+
+/* 首页 */
+router.get('/', function (req, res, next) {
+  var query = 'SELECT * FROM article';
+  mysql.query(query, function (err, rows, fields) {
+    var articles = rows;
+    articles.forEach(ele => {
+      var year = ele.articleTime.getFullYear();
+      var month = ele.articleTime.getMonth() + 1 > 10 ? ele.articleTime.getMonth() : '0' + ele.articleTime.getMonth();
+      var date = ele.articleTime.getDate() + 1 > 10 ? ele.articleTime.getDate() : '0' + ele.articleTime.getDate();
+      ele.articleTime = year + '-' + month + '-' + date
+    });
+    res.render('index', { articles: articles })
+  })
+});
+
+/* 文章内容页 */
+router.get('/article/:articleID', function (req, res, next) {
+  var articleID = req.params.articleID;
+  var query = 'SELECT * FROM article WHERE articleID=' + mysql.escape(articleID);
+  mysql.query(query, function (err, rows, fields) {
+    if (err) {
+      console.log(err);
+      return
+    }
+    // console.log(rows)
+    var query = 'UPDATE article SET articleClick=articleClick+1 WHERE articleID=' + mysql.escape(articleID)
+    mysql.query(query, function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+        return
+      }
+      var article = rows[0];
+      var year = article.articleTime.getFullYear();
+      var month = article.articleTime.getMonth() + 1 > 10 ? article.articleTime.getMonth() : '0' + article.articleTime.getMonth();
+      var date = article.articleTime.getDate() + 1 > 10 ? article.articleTime.getDate() : '0' + article.articleTime.getDate();
+      article.articleTime = year + '-' + month + '-' + date
+      res.render('article', { article: article })
+    })
   })
 })
 
